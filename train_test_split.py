7710bf868470e95871286271e4457e1098514ddb
elvinaang <13517079@std.stei.itb.ac.id>
2020-03-20 18:59:17 +0700

    update jupyter

--- train_test_split.py
+++ train_test_split.py
@@ -28,22 +28,21 @@ def ConfusionMatrixMLP(result_model, test_iris):
     # membuat confusion matrix dari sklearn
     return confusion_matrix(target_results, results, labels=['Virginica', 'Versicolor', 'Setosa'])
 
-def ID3(df):
+def ID3(df, pruning, is_result_continuous):
     # memisahkan data training dan testing dengan perbandingan 9:1
     separator_iris = round((9/10)*len(df.index))
     train_iris = df.iloc[:separator_iris, :].reset_index(drop = True)
     test_iris = df.iloc[separator_iris:, :].reset_index(drop = True)
 
     # pembelajaran dengan training data
-    result_tree = id3.ID3(df, train_iris, test_iris, True)
+    result_tree = id3.ID3(df, train_iris, test_iris, pruning)
 
     # menghitung kinerja
-    result_accuracy = id3.count_accuracy(result_tree, [], test_iris, True)
+    result_accuracy = id3.count_accuracy(result_tree, [], test_iris, is_result_continuous)
 
     # menampilkan tree hasil
     id3.print_tree(result_tree, 0)
-    print('Akurasi :')
-    print(result_accuracy)
+    print('Akurasi :', result_accuracy)
     print('Confussion Matrix untuk Virginica, Versicolor, Setosa :')
     print(ConfusionMatrixID3(result_tree, test_iris))
 
