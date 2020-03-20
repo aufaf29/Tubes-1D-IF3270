868436a6c00684b9e7ec7a427213c57bbb284541
Saskia Imani <imani.saskia@gmail.com>
2020-03-20 12:52:22 +0700

    Added kfold for MLP

--- kfold.py
+++ kfold.py
@@ -26,7 +26,7 @@ def ID3(k, df, pruning, is_result_continuous):
 
         df_validation = df.iloc[first_idx_val:last_idx_val, :].reset_index(drop = True)
 
-        print("-------------------- MODEL", i, "--------------------")
+        print("-------------------- MODEL", i+1, "--------------------")
 
         # We use the whole training data for pruning validation
         result_tree = id3.ID3(df, df_training, df_training, pruning)
@@ -44,8 +44,55 @@ def ID3(k, df, pruning, is_result_continuous):
     best_tree_idx = 0
     best_accuracy = trees_accuracy[0]
     for i in range(k):
-        if (trees_accuracy[i] < best_accuracy):
+        if (trees_accuracy[i] > best_accuracy):
             best_tree_idx = i
-            best_tree_idx = trees_accuracy[i]
+            best_accuracy = trees_accuracy[i]
 
     return trees[best_tree_idx]
+
+def MLP(k, df, num_perceptrons_in_layer, max_iteration, error_threshold, learning_rate, batch_size):
+    
+    num_data_per_fold = round(len(df.index) / k)
+    models = []
+    models_accuracy = []
+    
+    # Creating models and counting accuracy
+    for i in range(k):
+        first_idx_val = i * num_data_per_fold
+        last_idx_val = (i+1) * num_data_per_fold - 1
+
+        if (first_idx_val != 0 and last_idx_val != (len(df.index) - 1)):
+            df_training_top = df.iloc[:first_idx_val, :].reset_index(drop = True)
+            df_training_bottom = df.iloc[(last_idx_val + 1):, :].reset_index(drop = True)
+            df_training = pandas.concat([df_training_top, df_training_bottom])
+        elif (first_idx_val == 0 and last_idx_val != (len(df.index) - 1)):
+            df_training = df.iloc[(last_idx_val + 1):, :].reset_index(drop = True)
+        elif (first_idx_val != 0 and last_idx_val == (len(df.index) - 1)):
+            df_training = df.iloc[:first_idx_val, :].reset_index(drop = True)
+        else:
+            df_training = pandas.DataFrame()
+
+        df_validation = df.iloc[first_idx_val:last_idx_val, :].reset_index(drop = True)
+
+        print("-------------------- MODEL", i+1, "--------------------")
+
+        result_model = mlp.MLP(df_training, num_perceptrons_in_layer, max_iteration, error_threshold, learning_rate, batch_size)
+        models.append(result_model)
+
+        result_accuracy = mlp.count_accuracy(result_model, df_validation)
+        models_accuracy.append(result_accuracy)
+    
+        # Printing each model and its accuracy
+        
+        models[i].print_model()
+        print("ACCURACY:", models_accuracy[i])
+        print("")
+
+    best_model_idx = 0
+    best_accuracy = models_accuracy[0]
+    for i in range(k):
+        if (models_accuracy[i] > best_accuracy):
+            best_model_idx = i
+            best_accuracy = models_accuracy[i]
+
+    return models[best_model_idx]
